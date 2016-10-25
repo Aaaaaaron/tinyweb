48a54718fe86f50e811ed3b73853bbfce932abc5
tttMelody <245915794@qq.com>
2016-10-25 21:44:05 +0800

    complete

--- src/main/java/TinyWeb.java
+++ src/main/java/TinyWeb.java
@@ -24,6 +24,7 @@ public class TinyWeb {
             currentRequest = filter.doFilter( currentRequest );
         }
 
+        //找到对应的controller去处理请求 一个请求可以被多个拦截器拦截,但是只能由一个controller处理
         Controller controller = controllers.get( currentRequest.getPath() );
             if ( null == controller )
                 return null;
